088cd4e60e9f048ffacd341fd80312565593d950
vadimlelik <promiti90@gmail.com>
2024-11-26 13:08:55 +0300

    test 2

--- next.config.js
+++ next.config.js
@@ -4,6 +4,22 @@ const isPhoneSubdomain = process.env.NEXT_PUBLIC_PHONE === 'true'
 
 const nextConfig = {
 	assetPrefix: isPhoneSubdomain ? 'http://phone.cvirko-vadim.ru' : '',
+	async headers() {
+		return [
+			{
+				source: '/:path*',
+				headers: [
+					{ key: 'Access-Control-Allow-Origin', value: '*' },
+					{ key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+					{
+						key: 'Access-Control-Allow-Headers',
+						value: 'Content-Type, Authorization',
+					},
+					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+				],
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
