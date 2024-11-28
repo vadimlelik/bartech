428e4d508483e15ec4441b8888a0cd80b2519721
vadim.cvirko <promiti90@gmail.com>
2024-11-28 09:14:41 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const isPhoneSubdomain = process.env.NEXT_PUBLIC_PHONE === 'true'
 const nextConfig = {
 	assetPrefix: isPhoneSubdomain ? 'https://cvirko-vadim.ru/' : '',
 	reactStrictMode: false,
+	async rewrites() {
+		return [
+			{
+				source: '/phone/:path*',
+				destination: '/:path*',
+			},
+			{
+				source: '/tv1/:path*',
+				destination: '/:path*',
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
--- src/middleware.js
+++ src/middleware.js
@@ -2,18 +2,24 @@ import { NextResponse } from 'next/server'
 
 export function middleware(req) {
 	const url = req.nextUrl.clone()
-	const hostname = req.headers.get('host')?.split(':')[0] // Извлечение хоста
-	console.log('Hostname:', hostname) // Логирование хоста
+	const hostname = req.headers.get('host') // Получаем hostname
 
+	console.log('Hostname:', hostname)
+
+	// Проверяем, если запрос идет на статические файлы или системные пути Next.js
+	const staticPaths = ['/static', '/_next', '/favicon.ico']
+	if (staticPaths.some((path) => url.pathname.startsWith(path))) {
+		return NextResponse.next()
+	}
+
+	// Добавляем префиксы для поддоменов
 	if (hostname === 'phone.cvirko-vadim.ru') {
 		url.pathname = `/phone${url.pathname}`
-		console.log('Redirecting to:', url.toString())
 		return NextResponse.rewrite(url)
 	}
 
 	if (hostname === 'tv1.cvirko-vadim.ru') {
 		url.pathname = `/tv1${url.pathname}`
-		console.log('Redirecting to:', url.toString())
 		return NextResponse.rewrite(url)
 	}
 
