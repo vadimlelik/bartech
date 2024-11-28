c0b74f5d1fef6360b103d714b262be242e09960b
vadim.cvirko <promiti90@gmail.com>
2024-11-28 08:10:11 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isPhoneSubdomain = process.env.NEXT_PUBLIC_PHONE === 'true'
 
 const nextConfig = {
-	// assetPrefix: isPhoneSubdomain ? 'https://cvirko-vadim.ru/' : '',
+	assetPrefix: isPhoneSubdomain ? 'https://cvirko-vadim.ru/' : '',
 	reactStrictMode: false,
 }
 
