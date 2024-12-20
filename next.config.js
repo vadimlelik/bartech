679b064eb2ca13b129fb8349f6be3cb980001232
Vadim Cvirko <promiti90@gmail.com>
2024-12-20 17:16:19 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,57 +1,64 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-            },
-        ],
-    },
-    experimental: {
-        serverActions: {
-            bodySizeLimit: '2mb',
-        },
-    },
-    async rewrites() {
-        return [
-            // Правила для лендингов
-            {
-                source: '/1phonefree',
-                destination: '/(landing)/1phonefree'
-            },
-            {
-                source: '/1phonefree/:path*',
-                destination: '/(landing)/1phonefree/:path*'
-            },
-            {
-                source: '/phone2',
-                destination: '/(landing)/phone2'
-            },
-            {
-                source: '/phone2/:path*',
-                destination: '/(landing)/phone2/:path*'
-            },
-            {
-                source: '/tv1',
-                destination: '/(landing)/tv1'
-            },
-            {
-                source: '/tv1/:path*',
-                destination: '/(landing)/tv1/:path*'
-            },
-            {
-                source: '/50discount',
-                destination: '/(landing)/50discount'
-            },
-            {
-                source: '/50discount/:path*',
-                destination: '/(landing)/50discount/:path*'
-            }
-        ]
-    }
+	reactStrictMode: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**',
+			},
+		],
+	},
+	experimental: {
+		serverActions: {
+			bodySizeLimit: '2mb',
+		},
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: 'phone2.cvirko-vadim.ru',
+					},
+				],
+				destination: '/phone2/:path*',
+			},
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: 'tv1.cvirko-vadim.ru',
+					},
+				],
+				destination: '/tv1/:path*',
+			},
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: '1phonefree.cvirko-vadim.ru',
+					},
+				],
+				destination: '/1phonefree/:path*',
+			},
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: '50discount.cvirko-vadim.ru',
+					},
+				],
+				destination: '/50discount/:path*',
+			},
+		]
+	},
 }
 
 export default nextConfig
