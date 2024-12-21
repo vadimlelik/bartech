b8ebc1124788e424b3c875bf3f9fae5ab090b97f
Vadim Cvirko <promiti90@gmail.com>
2024-12-22 00:39:54 +0300

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -202,6 +202,18 @@ export default function Footer() {
 							</Box>
 						</Box>
 					</Grid>
+
+					{/* Правовая информация */}
+					<Grid item xs={12}>
+						<Typography
+							variant="body2"
+							color="text.secondary"
+							align="center"
+							sx={{ mt: 2 }}
+						>
+							Интернет-сайт носит исключительно информационный характер и ни при каких условиях не является публичной офертой, определяемой положениями ст. 405 Гражданского кодекса Республики Беларусь.
+						</Typography>
+					</Grid>
 				</Grid>
 			</Container>
 		</Box>
