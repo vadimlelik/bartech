6af2667dc9f7adba93ffe25e2a0575532ff3ef1d
Vadim Cvirko <promiti90@gmail.com>
2024-12-21 09:33:17 +0300

    fix next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
+const isPhoneSubdomain = process.env.NEXT_PUBLIC_PHONE === 'true'
+
 const nextConfig = {
+	assetPrefix: isPhoneSubdomain ? 'https://cvirko-vadim.ru/' : '',
 	reactStrictMode: false,
-	compiler: {
-		styledComponents: true
-	},
 	images: {
 		remotePatterns: [
 			{
@@ -21,44 +21,16 @@ const nextConfig = {
 	async rewrites() {
 		return [
 			{
-				source: '/:path*',
-				has: [
-					{
-						type: 'host',
-						value: 'phone2.cvirko-vadim.ru',
-					},
-				],
-				destination: '/phone2/:path*',
+				source: '/phone/:path*',
+				destination: '/:path*',
 			},
 			{
-				source: '/:path*',
-				has: [
-					{
-						type: 'host',
-						value: 'tv1.cvirko-vadim.ru',
-					},
-				],
-				destination: '/tv1/:path*',
+				source: '/phone',
+				destination: '/',
 			},
 			{
-				source: '/:path*',
-				has: [
-					{
-						type: 'host',
-						value: '1phonefree.cvirko-vadim.ru',
-					},
-				],
-				destination: '/1phonefree/:path*',
-			},
-			{
-				source: '/:path*',
-				has: [
-					{
-						type: 'host',
-						value: '50discount.cvirko-vadim.ru',
-					},
-				],
-				destination: '/50discount/:path*',
+				source: '/tv1/:path*',
+				destination: '/:path*',
 			},
 		]
 	},
