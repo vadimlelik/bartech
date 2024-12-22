80359d87fb802032f3a8ea09b053df84e9b8f2fd
Vadim Cvirko <promiti90@gmail.com>
2024-12-22 16:48:43 +0300

    fix 7

--- next.config.js
+++ next.config.js
@@ -1,67 +1,39 @@
 /** @type {import('next').NextConfig} */
 
+const isPhoneSubdomain = process.env.NEXT_PUBLIC_PHONE === 'true'
+
 const nextConfig = {
-    output: 'standalone',
-    poweredByHeader: false,
-    compress: true,
-
-    // Image configuration
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**.cvirko-vadim.ru',
-            }
-        ],
-        domains: ['cvirko-vadim.ru'],
-    },
-
-    // Rewrite rules for subdomains
-    async rewrites() {
-        return [
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'host',
-                        value: 'phone2.cvirko-vadim.ru',
-                    },
-                ],
-                destination: '/phone2/:path*',
-            },
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'host',
-                        value: 'tv1.cvirko-vadim.ru',
-                    },
-                ],
-                destination: '/tv1/:path*',
-            },
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'host',
-                        value: '1phonefree.cvirko-vadim.ru',
-                    },
-                ],
-                destination: '/1phonefree/:path*',
-            },
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'host',
-                        value: '50discount.cvirko-vadim.ru',
-                    },
-                ],
-                destination: '/50discount/:path*',
-            },
-        ]
-    },
+	assetPrefix: isPhoneSubdomain ? 'https://cvirko-vadim.ru/' : '',
+	reactStrictMode: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**',
+			},
+		],
+	},
+	experimental: {
+		serverActions: {
+			bodySizeLimit: '2mb',
+		},
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/phone/:path*',
+				destination: '/:path*',
+			},
+			{
+				source: '/phone',
+				destination: '/',
+			},
+			{
+				source: '/tv1/:path*',
+				destination: '/:path*',
+			},
+		]
+	},
 }
 
-module.exports = nextConfig
+export default nextConfig
