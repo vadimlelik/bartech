c32b52b9d718768000e9a6c450fafd0a4c5b1db0
Vadim Cvirko <promiti90@gmail.com>
2024-12-22 15:56:00 +0300

    add 2

--- next.config.js
+++ next.config.js
@@ -1,69 +1,65 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	reactStrictMode: false,
-	compiler: {
-		styledComponents: true,
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**',
-			},
-		],
-		unoptimized: true,
-	},
-	experimental: {
-		serverActions: {
-			bodySizeLimit: '2mb',
-		},
-	},
-	async assetPrefix() {
-		return;
-	},
-	async rewrites() {
-		return [
-			{
-				source: '/:path*',
-				has: [
-					{
-						type: 'host',
-						value: 'phone2.cvirko-vadim.ru',
-					},
-				],
-				destination: '/phone2/:path*',
-			},
-			{
-				source: '/:path*',
-				has: [
-					{
-						type: 'host',
-						value: 'tv1.cvirko-vadim.ru',
-					},
-				],
-				destination: '/tv1/:path*',
-			},
-			{
-				source: '/:path*',
-				has: [
-					{
-						type: 'host',
-						value: '1phonefree.cvirko-vadim.ru',
-					},
-				],
-				destination: '/1phonefree/:path*',
-			},
-			{
-				source: '/:path*',
-				has: [
-					{
-						type: 'host',
-						value: '50discount.cvirko-vadim.ru',
-					},
-				],
-				destination: '/50discount/:path*',
-			},
-		]
-	},
+    reactStrictMode: false,
+    compiler: {
+        styledComponents: true,
+    },
+    images: {
+        unoptimized: true,
+        domains: ['cvirko-vadim.ru', 'phone.cvirko-vadim.ru', 'phone2.cvirko-vadim.ru'],
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '2mb',
+        },
+    },
+    output: 'standalone',
+    async assetPrefix() {
+        return;
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'phone2.cvirko-vadim.ru',
+                    },
+                ],
+                destination: '/phone2/:path*',
+            },
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'tv1.cvirko-vadim.ru',
+                    },
+                ],
+                destination: '/tv1/:path*',
+            },
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: '1phonefree.cvirko-vadim.ru',
+                    },
+                ],
+                destination: '/1phonefree/:path*',
+            },
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: '50discount.cvirko-vadim.ru',
+                    },
+                ],
+                destination: '/50discount/:path*',
+            },
+        ]
+    },
 }
