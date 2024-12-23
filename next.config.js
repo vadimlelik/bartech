49efba93c29fdf4b47386dc57d9e9cdb531c2ffd
Vadim Cvirko <promiti90@gmail.com>
2024-12-23 11:28:13 +0300

    technobar.by

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isPhoneSubdomain = process.env.NEXT_PUBLIC_PHONE === 'true'
 
 const nextConfig = {
-	assetPrefix: isPhoneSubdomain ? 'https://cvirko-vadim.ru/' : '',
+	assetPrefix: isPhoneSubdomain ? 'https://technobar.by/' : '',
 	reactStrictMode: false,
 	images: {
 		remotePatterns: [
--- src/middleware.js
+++ src/middleware.js
@@ -1,47 +1,47 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-    const url = request.nextUrl
-    const hostname = request.headers.get('host')
+	const url = request.nextUrl
+	const hostname = request.headers.get('host')
 
-    // Карта соответствия поддоменов и путей
-    const subdomainMap = {
-        'phone2.cvirko-vadim.ru': '/phone2',
-        'tv1.cvirko-vadim.ru': '/tv1',
-        '1phonefree.cvirko-vadim.ru': '/1phonefree',
-        '50discount.cvirko-vadim.ru': '/50discount',
-        'phone.cvirko-vadim.ru': '/phone'
-    }
+	// Карта соответствия поддоменов и путей
+	const subdomainMap = {
+		'phone2.technobar.by': '/phone2',
+		'tv1.technobar.by': '/tv1',
+		'1phonefree.technobar.by': '/1phonefree',
+		'50discount.technobar.by': '/50discount',
+		'phone.technobar.by': '/phone',
+	}
 
-    // Проверяем, есть ли поддомен в нашей карте
-    if (subdomainMap[hostname]) {
-        // Создаем новый URL с правильным путем
-        const newUrl = new URL(request.url)
-        newUrl.pathname = `${subdomainMap[hostname]}${url.pathname}`
-        
-        // Логируем для отладки
-        console.log('Redirecting:', {
-            from: request.url,
-            to: newUrl.toString(),
-            hostname,
-            pathname: url.pathname
-        })
-        
-        return NextResponse.rewrite(newUrl)
-    }
+	// Проверяем, есть ли поддомен в нашей карте
+	if (subdomainMap[hostname]) {
+		// Создаем новый URL с правильным путем
+		const newUrl = new URL(request.url)
+		newUrl.pathname = `${subdomainMap[hostname]}${url.pathname}`
 
-    // Проверяем, если запрос идет на статические файлы или системные пути Next.js
-    const staticPaths = ['/static', '/_next', '/favicon.ico']
-    if (staticPaths.some((path) => url.pathname.startsWith(path))) {
-        return NextResponse.next()
-    }
+		// Логируем для отладки
+		console.log('Redirecting:', {
+			from: request.url,
+			to: newUrl.toString(),
+			hostname,
+			pathname: url.pathname,
+		})
 
-    return NextResponse.next()
+		return NextResponse.rewrite(newUrl)
+	}
+
+	// Проверяем, если запрос идет на статические файлы или системные пути Next.js
+	const staticPaths = ['/static', '/_next', '/favicon.ico']
+	if (staticPaths.some((path) => url.pathname.startsWith(path))) {
+		return NextResponse.next()
+	}
+
+	return NextResponse.next()
 }
 
 export const config = {
-    matcher: [
-        // Исключаем системные пути
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    ],
+	matcher: [
+		// Исключаем системные пути
+		'/((?!api|_next/static|_next/image|favicon.ico).*)',
+	],
 }
