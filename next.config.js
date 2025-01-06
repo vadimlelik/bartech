c0019fb50a381a34c655944934b929be18c17844
Vadim Cvirko <promiti90@gmail.com>
2025-01-06 17:07:48 +0300

    fix cvirko-vadim

--- next.config.js
+++ next.config.js
@@ -1,39 +1,39 @@
 /** @type {import('next').NextConfig} */
 
-const isPhoneSubdomain = process.env.NEXT_PUBLIC_PHONE === 'true'
+const isPhoneSubdomain = process.env.NEXT_PUBLIC_PHONE === 'true';
 
 const nextConfig = {
-	assetPrefix: isPhoneSubdomain ? 'https://technobar.by/' : '',
-	reactStrictMode: false,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**',
-			},
-		],
-	},
-	experimental: {
-		serverActions: {
-			bodySizeLimit: '2mb',
-		},
-	},
-	async rewrites() {
-		return [
-			{
-				source: '/phone/:path*',
-				destination: '/:path*',
-			},
-			{
-				source: '/phone',
-				destination: '/',
-			},
-			{
-				source: '/tv1/:path*',
-				destination: '/:path*',
-			},
-		]
-	},
-}
+  assetPrefix: isPhoneSubdomain ? 'https://cvirko-vadim.ru' : '',
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/phone/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/phone',
+        destination: '/',
+      },
+      {
+        source: '/tv1/:path*',
+        destination: '/:path*',
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
