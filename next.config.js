ca35b5ab75e24af489c1dae9733a9bf0fa988f9d
Vadim Cvirko <promiti90@gmail.com>
2025-01-07 13:51:40 +0300

    fix 3

--- next.config.js
+++ next.config.js
@@ -1,11 +1,62 @@
 /** @type {import('next').NextConfig} */
+
+const isPhoneSubdomain = process.env.NEXT_PUBLIC_PHONE === 'true';
+
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+  assetPrefix: isPhoneSubdomain ? 'https://cvirko-vadim.ru' : '',
+  reactStrictMode: false,
   images: {
-    domains: ['cvirko-vadim.ru', '*.cvirko-vadim.ru'], // Добавьте ваши домены
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/phone/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/phone',
+        destination: '/',
+      },
+      {
+        source: '/tv1/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/phone2/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/phone3/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/phone4/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/phone5/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/1phonefree/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/50discount/:path*',
+        destination: '/:path*',
+      },
+    ];
   },
 };
 
---
+++ src/middleware.js
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  const url = request.nextUrl;
+  const hostname = request.headers.get('host');
+
+  // Карта соответствия поддоменов и путей
+  const subdomainMap = {
+    'phone2.cvirko-vadim.ru': '/phone2',
+    'tv1.cvirko-vadim.ru': '/tv1',
+    '1phonefree.cvirko-vadim.ru': '/1phonefree',
+    '50discount.cvirko-vadim.ru': '/50discount',
+    'phone.cvirko-vadim.ru': '/phone',
+    'phon3.cvirko-vadim.ru': '/phone3',
+    'phon4.cvirko-vadim.ru': '/phone4',
+    'phon5.cvirko-vadim.ru': '/phone5',
+  };
+
+  // Проверяем, есть ли поддомен в нашей карте
+  if (subdomainMap[hostname]) {
+    // Создаем новый URL с правильным путем
+    const newUrl = new URL(request.url);
+    newUrl.pathname = `${subdomainMap[hostname]}${url.pathname}`;
+
+    // Логируем для отладки
+
+    return NextResponse.rewrite(newUrl);
+  }
+
+  // Проверяем, если запрос идет на статические файлы или системные пути Next.js
+  const staticPaths = ['/static', '/_next', '/favicon.ico'];
+  if (staticPaths.some((path) => url.pathname.startsWith(path))) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    // Исключаем системные пути
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
