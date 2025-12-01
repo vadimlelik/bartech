dac4cbf8a165dd7909fdfab0fce5538191f44d29
vadim.cvirko <promiti90@gmail.com>
2025-12-01 15:05:12 +0300

    fix roboto

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,4 +1,4 @@
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://bartech.by';
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://cvirko-vadim.ru';
 
 export default function robots() {
   return {
