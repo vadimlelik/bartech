9c47a7a10a24d2426a6773775eed8fbdbd5d6085
vadim.cvirko <promiti90@gmail.com>
2025-12-01 14:24:24 +0300

    fix roboto

--- src/app/robots.js
+++ src/app/robots.js
@@ -2,19 +2,20 @@ const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://bartech.by';
 
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/api/',
-        '/admin/',
-        '/checkout/',
-        '/cart',
-        '/favorites',
-        '/compare',
-      ],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/admin/',
+          '/checkout/',
+          '/cart',
+          '/favorites',
+          '/compare',
+        ],
+      },
+    ],
     sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
-
