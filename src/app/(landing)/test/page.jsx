5c47fad39eba466c4d58b06f2d421e7681fa4c99
vadimcvirko <cvi-vadim@yandex.ru>
2026-04-05 22:33:10 +0300

    test cicd

--- src/app/(landing)/test/page.jsx
+++ src/app/(landing)/test/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TestPage = () => {
-  return <div>TestPage TestPage hello world</div>;
+  return <div>TestPage TestPage hello world123</div>;
 };
 
 export default TestPage;
