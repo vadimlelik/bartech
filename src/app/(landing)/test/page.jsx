34345164e8741cc81d12fb77267bd6c4f4b4dba2
vadimcvirko <cvi-vadim@yandex.ru>
2026-04-05 22:40:52 +0300

    "fix: ci deploy"

--- src/app/(landing)/test/page.jsx
+++ src/app/(landing)/test/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TestPage = () => {
-  return <div>TestPage TestPage hello world123</div>;
+  return <div>TestPaывфge TestPage hello world123</div>;
 };
 
 export default TestPage;
