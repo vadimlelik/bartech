4de7dfa700e8beeb7f306ae175261e5b5dffb0c8
vadimcvirko <cvi-vadim@yandex.ru>
2026-04-06 10:10:56 +0300

    test page

--- src/app/(landing)/test/page.jsx
+++ src/app/(landing)/test/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TestPage = () => {
-  return <div>TestPaывфge TestPage hello world123</div>;
+  return <div>TestPaывфge TestPagвыаыаыаываыe hello world123</div>;
 };
 
 export default TestPage;
