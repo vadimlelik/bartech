9841f5507d26025d634915010dae52610c80ad82
vadimcvirko <cvi-vadim@yandex.ru>
2026-04-08 10:10:15 +0300

    fix SEO

---
+++ src/app/(landing)/phone7/layout.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'Смартфоны в рассрочку до 5 лет | Technobar',
+  description:
+    'Новый смартфон в рассрочку до 5 лет без справок о доходах и первого взноса. Более 1000 моделей в наличии, второй телефон в подарок и бесплатная доставка по Беларуси.',
+};
+
+export default function Phone7Layout({ children }) {
+  return children;
+}
